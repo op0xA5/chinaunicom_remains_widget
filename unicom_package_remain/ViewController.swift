b804e5aa470b39f0ffeca0b984f86d6f4201e38a
zhujinliang <me@zhujinliang.com>
2017-02-08 11:32:57 +0800

    Initial Commit

---
+++ unicom_package_remain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unicom_package_remain
+//
+//  Created by zhujl on 2/8/17.
+//  Copyright © 2017 zhujl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
